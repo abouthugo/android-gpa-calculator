76f2b7c11eba076413b2a1b215a3abea13182d39
Hugo Perdomo <hugo@Hugos-Mac18.home>
2019-02-17 15:10:54 -0500

    Scrollable, works on landscape

--- app/src/main/java/com/example/gpa_10894634_calculator/MainActivity.java
+++ app/src/main/java/com/example/gpa_10894634_calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import java.util.ArrayList;
 
